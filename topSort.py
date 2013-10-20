f33c470e1f66a419faa03f265e02f799f60990d6
Matt Dumford <mdumford99@gmail.com>
2013-10-20 15:44:32 -0500

    topsort works for graph a

--- topSort.py
+++ topSort.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python
 
-def calcDegrees():
-	pass
+def calcDegrees(): #calculate degrees of all nodes
+	for i in graph:
+		if i is not None:
+			for j in range(1, len(i)):
+				inDegrees[i[j]] += 1
 
-def removeNode(n):
-	pass
+	for i in range(1, len(inDegrees)):
+		if inDegrees[i]==0:
+			if i not in degreeZero:
+				degreeZero.append(i)
 
-graph = [[1,2,3,4],[2,3,5],[3,4,5],[4,5],[5]]
 
-degreeZero = []
-inDegrees = []
+def removeNode(n): #removes node n from all lists
+	degreeZero.remove(n)
+
+	inDegrees[n] = None
+	for i in range(len(inDegrees)):
+		if inDegrees[i] is not None:
+			inDegrees[i] = 0
+
+	graph[n-1] = None
+	for i in range(len(graph)):
+		if graph[i] is not None:
+			try:
+				graph[i].remove(n)
+			except ValueError:
+				pass
+
+
+
+
+graph = [[1,2,3,4],[2,3,5],[3,4,5],[4,5],[5]] #graph to sort
+print "Graph: ", graph
+
+degreeZero = [] #queue of nodes with degree 0
+inDegrees = [None] + [0] * len(graph) #there is no 0 node
 ans = []
 
 calcDegrees()
@@ -19,4 +45,4 @@ while len(degreeZero) > 0:
 	removeNode(degreeZero[0])
 	calcDegrees()
 
-print(ans)
+print "Sorted: ", ans
